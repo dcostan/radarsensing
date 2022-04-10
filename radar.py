a47e99f28f869f540592b044200a18825f7b6fa1
dcostan <davide.cst00@gmail.com>
2022-04-10 17:38:53 +0200

    Add files via upload

--- radar.py
+++ radar.py
@@ -2,45 +2,52 @@ import numpy
 from random import randint
 
 class Sensor:
+
     def __init__(self, t, R, D, opening, range):
         self.t = t
         self.R = R
         self.D = D
         self.opening = opening
         self.range = range
+        self.track_data = []
+    
+    def point_incoming(self, point):
+        if 1 == 1:
+            self.track_data.append(point)
+        else:
+            self.track_data.append([])
 
-def fake_track_gen(width, height, x0, y0, t):
-    track = []
-    track.append([x0, y0])
-    for i in list(range(t)):
-        if i != 0:
-            xp = track[i-1][0]
-            yp = track[i-1][1]
-            
-            p=[0.6, 0.2, 0.2]
-            
-            if 1 == 1:
-                xrange = [1, 0, -1]
-                yrange = [1, 0, -1]
-            
-            if xp == 0:
-                xm = randint(0, 1)
-            elif xp == width:
-                xm = randint(-1, 0)
-            else:
-                xm = numpy.random.choice(xrange, p=p)
-            
-            if yp == 0:
-                ym = randint(0, 1)
-            elif yp == height:
-                ym = randint(-1, 0)
-            else:
-                ym = numpy.random.choice(yrange, p=p)
-            
-            xn = xp + xm
-            yn = yp + ym
-            track.append([xn, yn])
-    return track
+class Central:
+
+    def __init__(self, width, height, t):
+        self.t = t
+        self.width = width
+        self.height = height
+        self.track = self.fake_track_gen(width, height, t)
+        self.position_counter = 0
+        self.current_position = self.track[self.position_counter]
+        
+    def fake_track_gen(self, width, height, t):
+        track = []
+        track.append([56, 29])
+        track.append([56, 30])
+        track.append([55, 31])
+        track.append([55, 32])
+        track.append([54, 33])
+        track.append([53, 34])
+        track.append([52, 35])
+        track.append([51, 36])
+        track.append([51, 37])
+        track.append([50, 38])
+        return track
+    
+    def generate_next(self):
+        self.position_counter = self.position_counter + 1
+        self.current_position = self.track[self.position_counter]
+    
+    def send_to_sensors(self, sensors):
+        for s in sensors:
+            s.point_incoming([self.current_position])
     
 # https://stackoverflow.com/questions/13430231/how-i-can-get-cartesian-coordinate-system-in-matplotlib
 # Lo scopo del programma è fare che?
@@ -54,4 +61,11 @@ def fake_track_gen(width, height, x0, y0, t):
 #   central.send_to_sensors()
 #   sensor.step()
 
-print(fake_track_gen(100, 100, 10, 10, 60))
+s1 = Sensor(1, 1, 1, 1, 1)
+central = Central(100, 100, 10)
+
+central.send_to_sensors([s1])
+central.generate_next()
+central.send_to_sensors([s1])
+
+print(s1.track_data)
