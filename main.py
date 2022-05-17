43fafae54683ef9aed1f0d82cbbcdb499093c55c
PablosMarco <canil.marco@gmail.com>
2022-05-17 21:33:49 +0200

    corrected formulas and other coding stuff

--- main.py
+++ main.py
@@ -1,40 +1,80 @@
-from radar import *
+import numpy as np
+import matplotlib.pyplot as plt
 
-s1 = Sensor(np.array([-2, 2]), np.matrix([[0, 1], [-1, 0]]), 0, 45, 2)
-s2 = Sensor(np.array([-3, -6]), np.matrix([[1, 0], [0, 1]]), 0, 45, 2)
+from radar import Central, Sensor
+
+# sensor 1
+t1 = np.array([-5, 1]).reshape(2, 1)  # position (column vector)
+theta1 = np.deg2rad(-30)  # rotation
+R1 = np.array([[np.cos(theta1), -np.sin(theta1)],
+                [np.sin(theta1), np.cos(theta1)]])
+s1 = Sensor(t1, R1, 0, opening=120, range=8)
+
+# sensor 2
+theta2 = np.deg2rad(30)  # position (column vector)
+t2 = np.array([5, 2]).reshape(2, 1)  # rotation
+R2 = np.array([[np.cos(theta2), -np.sin(theta2)],
+                [np.sin(theta2), np.cos(theta2)]])
+s2 = Sensor(t2, R2, 0, opening=120, range=8)
+
+sensors = [s1, s2]
+
 central = Central()
 
-track_0 = { "id": 0, "pos": [ [1, 3],
-                              [1, 2],
-                              [55, 31],
-                              [55, 32],
-                              [54, 33],
-                              [53, 34],
-                              [52, 35],
-                              [51, 36],
-                              [51, 37],
-                              [50, 38]  ], "start_time": 0 }
+track_0 = {"id": 0, "pos": [[1, 3],
+                            [1, 2],
+                            [55, 31],
+                            [55, 32],
+                            [54, 33],
+                            [53, 34],
+                            [52, 35],
+                            [51, 36],
+                            [51, 37],
+                            [50, 38]], "start_time": 0}
 
-track_1 = { "id": 1, "pos": [ [0, 3], 
-                              [56, 30],
-                              [55, 31],
-                              [55, 32],
-                              [54, 33],
-                              [53, 34],
-                              [52, 35],
-                              [51, 36],
-                              [51, 37],
-                              [50, 38]  ], "start_time": 1 }
+track_1 = {"id": 1, "pos": [[0, 3],
+                            [56, 30],
+                            [55, 31],
+                            [55, 32],
+                            [54, 33],
+                            [53, 34],
+                            [52, 35],
+                            [51, 36],
+                            [51, 37],
+                            [50, 38]], "start_time": 0 }
+
+# generate track from (0, 0) to (0, 0.5*5^2) with an additional gaussian noise
+pos_x = np.linspace(start=0, stop=5, num=50).reshape(-1, 1)
+noise = np.random.normal(0, 0.5, 50).reshape(-1, 1)  # mean=0, std=0.5, 50 points
+pos_y = (0.5 * pos_x ** 2 + noise).reshape(-1, 1)
+pos = np.concatenate((pos_x, pos_y), axis=1).tolist()
+track_3 = {"id": 2, "pos": pos, "start_time": 1 }
 
 central.add_track(track_0)
 central.add_track(track_1)
+central.add_track(track_3)
 
-central.generate_next()
-central.send_to_sensors([s1])
-central.generate_next()
-central.send_to_sensors([s1])
+# run the system for some time
+for i in range(50):
+    central.generate_next()
+    central.send_to_sensors(sensors)
 
-print(central.trackobserver)
-print(s1.tracks_observed)
+ax = central.show_room([s1, s2]) # when you plot, you plot on an ax object, and you can plot multiple things on the same ax
 
-central.show_room([s1, s2])
+# plot all the tracks provided
+for track in central.tracks:
+    track_array = np.asarray(track["pos"])
+    track_id = track["id"]
+    ax.plot(track_array[:, 0], track_array[:, 1], '--', label=f"generated_track-{track_id}")
+
+# plot the tracks seen by the radars
+for sensor in sensors:
+    R = sensor.R
+    t = sensor.t
+    for track_id, track in sensor.tracks_observed.items():
+        track_array = (R @ np.asarray(track["pos"]).squeeze().T + t).T  # transform track from radar to abs ref sys
+        ax.plot(track_array[:, 0], track_array[:, 1], label=f"obs_track-{track_id}")
+
+plt.legend()
+plt.show()
+
--- radar.py
+++ radar.py
@@ -1,7 +1,7 @@
-from matplotlib.patches import Arc
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+
 
 class Sensor:
 
@@ -12,32 +12,41 @@ class Sensor:
         self.opening = opening
         self.range = range
         self.tracks_observed = {}
-    
+
     def add_point_to_track(self, track_id, point):
         if track_id not in self.tracks_observed.keys():
-            self.tracks_observed[track_id] = { "pos": [ point ] }
+            self.tracks_observed[track_id] = {"pos": [point]}
         else:
             self.tracks_observed[track_id]["pos"].append(point)
-    
-    def is_point_in_fov(self, point):
-        m1 = math.tan( ( 90 - self.opening ) * math.pi / 180 )
-        first_straight_condition = ( point.flat[1] > m1 * point.flat[0] )
-        
-        m2 = math.tan( ( 90 + self.opening ) * math.pi / 180 )
-        second_straight_condition = ( point.flat[1] > m2 * point.flat[0] )
 
-        return first_straight_condition and second_straight_condition
+    def is_point_in_fov(self, point):
+        point = np.array(point)
+        m1 = math.tan((90 - self.opening) * math.pi / 180)
+        first_straight_condition = (point.flat[1] > m1 * point.flat[0])
+
+        m2 = math.tan((90 + self.opening) * math.pi / 180)
+        second_straight_condition = (point.flat[1] > m2 * point.flat[0])
+
+        range_condition = np.linalg.norm(point) <= self.range  # TODO: limite sul range?
+
+        return first_straight_condition and second_straight_condition and range_condition
 
     def rel_coordinates(self, P_):
-        P = self.R @ P_ + [self.t]
-        return np.array(P)
-    
+        """Transforms point P_ from absolute reference system to the sensor reference system
+
+        :param P_: the point to be transformed; the point is assumed to be a column vector.
+        :type P_: np.ndarray
+        :returns: the transformed point"""
+        # P = self.R @ P_ + [self.t]  # TODO: P = self.R.T @ (P_ - [self.t])
+        P = self.R.T @ (P_ - self.t)
+        return P.squeeze().tolist()
+
     def points_incoming(self, trackobserver):
         for track_id in trackobserver.keys():
             P = self.rel_coordinates(trackobserver[track_id]["pos"])
             if self.is_point_in_fov(P):
                 self.add_point_to_track(track_id, P)
-            
+
 
 class Central:
 
@@ -45,51 +54,61 @@ class Central:
         self.tracks = []
         self.timestep = 0
         self.trackobserver = {}
-    
+
     def add_track(self, track):
         self.tracks.append(track)
-    
+
     def generate_next(self):
         self.trackobserver = {}
         for track in self.tracks:
-            if 0 <= self.timestep - track["start_time"] <= len(track["pos"]):
-                self.trackobserver[track["id"]] = { "pos": np.array(track["pos"][self.timestep-track["start_time"]]) }
+            if 0 <= self.timestep - track["start_time"] < len(track["pos"]):  # TODO: bisogna mettere < e non <= altrimenti va fuori array
+                self.trackobserver[track["id"]] = {
+                    "pos": np.array(track["pos"][self.timestep - track["start_time"]]).reshape(2, 1)}  # TODO: reshaped to column vector
         self.timestep = self.timestep + 1
-    
+
     def send_to_sensors(self, sensors):
         for s in sensors:
             s.points_incoming(self.trackobserver)
-            
+
     def abs_coordinates(self, s, P):
-        P_ = s.R.T @ ( P - [s.t] ).T
+        # P_ = s.R.T @ ( P - [s.t] ).T  # TODO: other way round
+        P_ = s.R @ P + s.t
         return np.array(P_)
-    
+
     def show_room(self, sensors):
         plt.rcParams["figure.figsize"] = [6.50, 6.50]
         plt.rcParams["figure.autolayout"] = True
-        
+
+        fig, ax = plt.subplots()  # this way you create an ax object that you can return and plot on it other things
+
         for sensor in sensors:
-        
-            origin = self.abs_coordinates(sensor, np.array([0,0])).flat
-            limit1 = self.abs_coordinates(sensor, np.array([math.cos(( 90 - ( sensor.opening/2) ) * math.pi / 180 ) * sensor.range,
-                                                            math.sin(( 90 - ( sensor.opening/2) ) * math.pi / 180 ) * sensor.range])).flat
-            limit2 = self.abs_coordinates(sensor, np.array([math.cos(( 90 + ( sensor.opening/2) ) * math.pi / 180 ) * sensor.range,
-                                                            math.sin(( 90 + ( sensor.opening/2) ) * math.pi / 180 ) * sensor.range])).flat
+            origin = self.abs_coordinates(sensor, np.array([[0], [0]])).flat  # vectors are columns
+            limit1 = self.abs_coordinates(sensor, np.array(
+                [[math.cos((90 - (sensor.opening / 2)) * math.pi / 180) * sensor.range],
+                 [math.sin((90 - (sensor.opening / 2)) * math.pi / 180) * sensor.range]]))
+            limit2 = self.abs_coordinates(sensor, np.array(
+                [[math.cos((90 + (sensor.opening / 2)) * math.pi / 180) * sensor.range],
+                 [math.sin((90 + (sensor.opening / 2)) * math.pi / 180) * sensor.range]]))
             x_line1 = [origin[0], limit1[0]]
             y_line1 = [origin[1], limit1[1]]
             x_line2 = [origin[0], limit2[0]]
             y_line2 = [origin[1], limit2[1]]
-            
-            rotation = np.degrees(np.arccos(sensor.R.flat[0]))
+
+            # rotation = np.degrees(np.arccos(sensor.R.flat[0]))  # TODO: il coseno è simmetrico rispetto all'asse x, quindi dopo 180° l'arccos "torna indietro"
+            rotation = np.degrees(
+                np.arctan2(sensor.R[1, 0], sensor.R[0, 0]))  # usando arctan2, tiene conto del quadrante corretto
             theta_zero = 90 - sensor.opening / 2
             theta_one = 90 + sensor.opening / 2
             t = np.linspace(theta_zero + rotation, theta_one + rotation, 360)
             x_arc = sensor.range * np.cos(np.radians(t)) + origin[0]
             y_arc = sensor.range * np.sin(np.radians(t)) + origin[1]
-            
-            plt.plot(x_arc, y_arc, 'b')
-            plt.plot(x_line1, y_line1, 'b', linestyle="-")
-            plt.plot(x_line2, y_line2, 'b', linestyle="-")
-            plt.text(origin[0]-0.015, origin[1]+0.05, "Radar")
 
-        plt.show()
+            ax.plot(x_arc, y_arc, 'b')
+            ax.plot(x_line1, y_line1, 'b', linestyle="-")
+            ax.plot(x_line2, y_line2, 'b', linestyle="-")
+            ax.text(origin[0] - 0.015, origin[1] + 0.05, "Radar")
+        ax.grid()
+        ax.set_xlim(-10, 10)  # for now I limited it this way... can be changed
+        ax.set_ylim(0, 20)
+
+        return ax
